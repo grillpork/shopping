1273e8e8513ca9b5565feea3882cd5664493d70b
Tripop Saibut <tripopsaibut@gmail.com>
2025-01-20 06:55:27 +0700

    updateA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,16 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-      <nav className=' flex justify-between items-center py-4 w-[80%] m-auto'>
-        <div className='inline-flex justify-center items-center space-x-8'>
-          <div className=' text-red-600 text-3xl font-bold'>Mall TH</div>
-          <ul className=' flex space-x-8'>
-              <Link className=' hover: text-blue-500 font-bold before: absolute top-10 
-                left-0 bg-gray-300 w-[50] h-[4] rounded-xl 
+      <nav className=' flex justify-between items-center py-4 px-8 m-auto'>
+          <ol className='flex justify-center items-center  gap-4'>
+            <Link to= {'/'} className='text-2xl font-bold'>LOGO</Link>
+              <Link className=' hover:text-blue-500 hover:translate-y-[-4px] 
               ' to= {'/'}>Home</Link>
-              <Link className=' hover: text-blue-500 font-bold before: absolute top-10 
-                left-0 bg-gray-300 w-[50] h-[4] rounded-xl 
+              <Link className=' hover:text-blue-500 hover:translate-y-[-4px]  
               ' to= {'/shop'}>Shop</Link>
-              <Link className=' hover: text-blue-500 font-bold before: absolute top-10 
-                left-0 bg-gray-300 w-[50] h-[4] rounded-xl 
-              ' to= {'/'}>New</Link>
-              <Link className=' hover: text-blue-500 font-bold before: absolute top-10 
-                left-0 bg-gray-300 w-[50] h-[4] rounded-xl 
-              ' to= {'/'}>Cart</Link>
-          </ul>
-        </div>
-
+              <Link className=' hover:text-blue-500 hover:translate-y-[-4px]  
+              ' to= {'/cart'}>Cart</Link>
+          </ol>
         <div className=' space-x-8'>
           <Link to= {'/login'}>Sign In</Link>
           <Link to= {'/register'}>Register</Link>
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const handleOnChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (form.password !== form.confirmPassword) {
+      return alert("Password doesn't match!");
+    }
+    console.log(form);
+
+    try {
+      const res = await axios.post('http://localhost:5173/api/register', form);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       Register
-    </div>
-  )
-}
 
-export default Register
+      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center gap-10'>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name='email'
+            onChange={handleOnChange}
+            className='text-black'
+          />
+        </div>
+
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name='password'
+            onChange={handleOnChange}
+            className='text-black'
+          />
+        </div>
+
+        <div>
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            name='confirmPassword'
+            onChange={handleOnChange}
+            className='text-black'
+          />
+        </div>
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
